5a18942f73ae3e2a5915fb9c200df71b3113d701
alswo1641 <alswo1641@naver.com>
2024-11-26 09:56:04 +0900

    fix: 버그수정

--- bin/main/page/controller/tetris_controller.js
+++ bin/main/page/controller/tetris_controller.js
@@ -14,8 +14,8 @@ class Tetris {
       this.tetrominoes = [
         { shape: [[1, 1, 1], [0, 1, 0]], color: "purple" },   
         { shape: [[1, 1], [1, 1]], color: "yellow" },         
-        { shape: [[0, 1], [0, 1], [1, 1]], color: "blue" },  
-        { shape: [[0, 1, 1], [1, 1, 0]], color: "green" },   
+        { shape: [[0, 1], [0, 1], [1, 1]], color: "blue" },   
+        { shape: [[0, 1, 1], [1, 1, 0]], color: "green" },    
       ];
     }
   
